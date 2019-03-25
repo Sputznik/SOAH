3621e3e77e3caa97cb03dbc90943c799cfa9c629
Stephen <anil.stephen66@gmail.com>
2019-03-25 13:44:48 +0530

    updated the version of stylesheet

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@ include( 'lib/lib.php' );
 add_theme_support( 'post-thumbnails' );
 //Load child stylesheet after parent stylesheet
 add_action('wp_enqueue_scripts', function(){
-  wp_enqueue_style( 'soah-child', get_stylesheet_directory_uri() .'/style.css', array( 'sp-core-style' ), time() );
-  wp_enqueue_style( 'soah-fep', get_stylesheet_directory_uri() .'/assets/css/fep.css', array( 'soah-child' ), time() );
-  wp_enqueue_script( 'contact-form-js', get_stylesheet_directory_uri().'/assets/js/form.js', array('jquery'), time(), true );
+  wp_enqueue_style( 'soah-child', get_stylesheet_directory_uri() .'/style.css', array( 'sp-core-style' ), '1.0.0' );
+  wp_enqueue_style( 'soah-fep', get_stylesheet_directory_uri() .'/assets/css/fep.css', array( 'soah-child' ), '1.0.0' );
+  wp_enqueue_script( 'contact-form-js', get_stylesheet_directory_uri().'/assets/js/form.js', array('jquery'), '1.0.0', true );
   wp_enqueue_script( 'recaptcha', 'https://www.google.com/recaptcha/api.js', array(), '1.0.0', true );
 });
 
