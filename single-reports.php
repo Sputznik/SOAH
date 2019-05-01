f622a08617fb023c2945a00c7c6418450d88af7a
unknown <samvthom16@gmail.com>
2019-05-01 15:00:01 +0530

    Include orbit-query templates in the theme

--- single-reports.php
+++ single-reports.php
@@ -4,48 +4,7 @@
     <div class="col-sm-12"><div class="paper-box" style="border: #eee solid 1px;padding: 25px;margin-top:20px;box-shadow:#eee 4px 5px 5px 2px;">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <h3><?php the_title();?></h3>
-        <?php
-
-          $metainfo = array(
-            array(
-              'icon'      => 'fa fa-calendar',
-              'label'     => '',
-              'shortcode' => '[orbit_date]'
-            ),
-            array(
-              'icon'      => 'fa fa-map-marker',
-              'label'     => '',
-              'shortcode' => '[orbit_terms taxonomy="locations"]'
-            ),
-            array(
-              'icon'      => 'fa fa-bell',
-              'label'     => 'Reported: &nbsp;',
-              'shortcode' => '[orbit_terms taxonomy="report-type"]'
-            ),
-            array(
-              'icon'      => 'fa fa-users',
-              'label'     => 'Affected Victims: &nbsp;',
-              'shortcode' => '[orbit_terms taxonomy="victims"]'
-            ),
-          );
-
-          foreach ($metainfo as $meta) {
-            if( $meta['shortcode'] ){
-
-              $value = do_shortcode( $meta['shortcode'] );
-
-              if( $value ){
-                _e("<p class='small'>");
-                if( $meta['icon'] ){ _e( "<i class='". $meta['icon'] ."'></i> &nbsp; " ); }
-                if( $meta['label'] ){ _e( "<label>". $meta['label'] ."</label>" ); }
-                echo $value;
-                _e("</p>");
-              }
-
-            }
-
-          }
-        ?>
+        <?php get_template_part("partials/content", "report");?>
         <hr>
         <?php the_content('Read the rest of this entry »'); ?>
         <hr>
