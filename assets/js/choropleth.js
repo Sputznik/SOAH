d6cfbc72a85942721c754e7be3bc2ecf2f09e059
unknown <samvthom16@gmail.com>
2019-05-04 14:21:01 +0530

    Add percentile to popup

--- assets/js/choropleth.js
+++ assets/js/choropleth.js
@@ -126,6 +126,8 @@
 						if( data[i]["reports"] == 1 ){ content += " incident"; }
 						else{ content += " incidents"; }
 
+						content += "(" + data[i]['percentile'] + ")";
+
 						report_types = getFormValues( 'tax_report-type[]' );
 						year = $el.find('form [name=cf_year]').val();
 						victims = getFormValues( 'tax_victims[]' );
@@ -292,7 +294,7 @@
 
 				createKeys();
 
-				
+
 
 			}
 
