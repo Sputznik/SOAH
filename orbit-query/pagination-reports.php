811b8d4aaba3d96e77ce6d37a0d758891e12c06a
unknown <samvthom16@gmail.com>
2019-05-16 10:31:50 +0530

    Fix pagination for reports

--- orbit-query/pagination-reports.php
+++ orbit-query/pagination-reports.php
@@ -1,30 +1,24 @@
 <?php if($atts['pagination'] != '0'):?>
-<?php //print_r( $atts );
-// the_posts_pagination(
-//   array(
-//     'mid_size'  =>  '2',
-//     'prev_text' =>  'Previous Posts',
-//     'next_text' =>  'Next'
-//   )
-// );
+<?php
 
   $paged = ( get_query_var('orbit-paged')) ? get_query_var('orbit-paged') : 1;
 
   $total_pages = $this->query->max_num_pages;
 
-
   if( $total_pages > 1 ){
 
-    $current_page = max( 1, get_query_var('orbit-paged') );
+    $current_page = max( 1, get_query_var('paged') );
 
-    echo paginate_links(array(
-      'base' => get_pagenum_link(1) . '%_%',
-      'format' => '?orbit-paged/%#%/',
-      'current' => $current_page,
-      'total' => $total_pages,
-      'prev_text'    => __('« Prev'),
-      'next_text'    => __('Next »'),
-    ));
+    $big = 9999999;
+
+    echo paginate_links( array(
+      'base'       => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	    'format'     => '?paged=%#%',
+      'current'    => $current_page,
+      'total'      => $total_pages,
+      'prev_text'  => __('« Prev'),
+      'next_text'  => __('Next »'),
+    ) );
   }
 
 ?>
