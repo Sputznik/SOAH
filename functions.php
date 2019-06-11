ef2948afdcc8b87742f071dad551ed6e018aa32a
unknown <samvthom16@gmail.com>
2019-06-11 09:55:15 +0530

    Css fix on map

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include( 'lib/lib.php' );
 add_theme_support( 'post-thumbnails' );
 
 //Constant changes all the js and css version on the go
-define( 'SOAH_VERSION', '1.6.9' );
+define( 'SOAH_VERSION', '1.7.0' );
 
 
 //Load child stylesheet after parent stylesheet
